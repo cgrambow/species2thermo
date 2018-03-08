8ced76c8c0f99ae0df18b30d448f177769bdbd4c
Colin Grambow <cgrambow@mit.edu>
2018-03-08 17:42:15 -0500

    Convert species to input files

--- s2t/gaussian.py
+++ s2t/gaussian.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
 
-def make_input(species_dict):
-    pass
+import ard.gen3D as gen3D
+from ard.quantum import Gaussian
+from rmgpy.chemkin import loadSpeciesDictionary
+from rmgpy.molecule.generator import toOBMol
+
+
+def make_input(species_dict, out_dir, settings):
+    species_dict = loadSpeciesDictionary(species_dict)
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    ignore = {'Ar', 'He', 'Ne', 'N2'}
+
+    for label, spc in species_dict.iteritems():
+        if label not in ignore:
+            node = species_to_node(spc)
+            g = Gaussian()
+            g.makeInputFile(node,
+                            name=label,
+                            jobtype='opt',
+                            output_dir=out_dir,
+                            **settings)
+
+
+def species_to_node(spc):
+    """
+    Convert an RMG species to an ARD node. Generate 3D coordinates in
+    the process.
+    """
+    rmgmol = spc.molecule[0]
+    obmol = toOBMol(rmgmol)
+    ardmol = gen3D.Molecule(obmol)
+    ardmol.addh()
+    ardmol.gen3D(make3D=True)
+    return ardmol.toNode()
--- scripts/make_input.py
+++ scripts/make_input.py
@@ -7,11 +7,17 @@ import argparse
 def main():
     parser = argparse.ArgumentParser('Converts a species dictionary to quantum chemistry input.')
     parser.add_argument('dict', metavar='DICT', help='Path to species dictionary')
+    parser.add_argument('out_dir', metavar='DIR', help='Directory to store output files in ')
+    parser.add_argument('-n', '--nproc', type=int, metavar='N', help='Number of processors in Gaussian input file')
+    parser.add_argument('-m', '--mem', type=int, metavar='MEM', help='Amount of memory in MB per job')
+    parser.add_argument('-t', '--theory', metavar='THEORY', help='Level of theory/basis set')
     args = parser.parse_args()
     species_dict = args.dict
+    out_dir = args.out_dir
+    settings = {'theory': args.theory, 'nproc': args.nproc, 'mem': str(args.mem) + 'mb'}
 
     from s2t.gaussian import make_input
-    make_input(species_dict)
+    make_input(species_dict, out_dir, settings)
 
 
 if __name__ == '__main__':
